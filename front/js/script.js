97f88ec6859025e1a2416b0082c0e2ec506e0ad9
Havvaozlk <ozcelik.havva@icloud.com>
2022-07-28 13:01:14 +0200

    récupération des données de l'api et affichage des produits de la page d'accueil

---
+++ front/js/script.js
@@ -0,0 +1,28 @@
+//fonction pour récupérer les données de l'API
+fetch("http://localhost:3000/api/products")
+.then((res) => {
+    if (res.ok) {
+        return res.json();
+    }
+})
+.then((data) => {
+    getProducts(data);
+})
+.catch((error) => {
+    console.log(error);
+})
+
+
+//AFFICHAGE DES PRODUITS
+function getProducts(data) {
+    for (product of data) {
+        let items = document.querySelector(".items");
+        items.innerHTML += `<a href="./product.html?${product._id}">
+        <article>
+          <img src="${product.imageUrl}" alt="${product.altTxt}">
+          <h3 class="productName">${product.name}</h3>
+          <p class="productDescription">${product.description}</p>
+        </article>
+      </a>`;
+    }
+}
