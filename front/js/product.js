b8bc2430eb0c7a2345d1726c1f91ea7ea8add9fe
Havvaozlk <ozcelik.havva@icloud.com>
2022-08-05 01:10:01 +0200

    récupération des données avec fetch

--- front/js/product.js
+++ front/js/product.js
@@ -1,29 +1,41 @@
 var str = window.location.href;
 var url = new URL(str);
 console.log(str);
-var idProduct = url.search.substring(1,33);
+var idProduct = url.search.substring(1);
 console.log(idProduct);
 
-//INSERER LE TITRE
-let title = document.querySelector('#title');
-title.innerHTML = product.name;
+let title = document.querySelector("#title");
+let price = document.querySelector("#price");
+let description = document.querySelector("#description");
+let colors = document.querySelector("#colors");
+let productImg = document.createElement('img');
+document.querySelector('.item__img').appendChild(productImg);
 
-//INSERER LE PRIX
-let price = document.querySelector('#price');
-price.innerHTML = product.price;
 
-//INSERER LA DESCRIPTION
-let description = document.querySelector('#description');
-description.innerHTML = product.description;
-
-//INSERER LES COULEURS
-let colors = document.querySelector('#colors');
-for ( let i = 0; i < product.colors.lenght; i++) {
-   let color = document.createElement('option');
-   colors.appendChild(color);
-   color.setAttribute('value', product.colors[i])
+async function getProduct() {
+    await fetch("http://localhost:3000/api/products/" + idProduct)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return 0;
+            }
+        })
+        .then((product) => {
+            title.innerHTML = product.name;
+            price.innerHTML = product.price;
+            description.innerHTML = product.description;
+            productImg.setAttribute('src', product.imageUrl);
+            productImg.setAttribute('alt', product.altTxt);
+            for (let i = 0; i < product.colors.length; i++) {
+                let color = document.createElement("option");
+                colors.appendChild(color);
+                color.setAttribute("value", product.colors[i]);
+                color.innerHTML = product.colors[i];
+                console.log(product.colors[i]);
+            }
+        })
+        .catch((error) => console.log("erreur :" + error));
 }
 
-
-
-
+getProduct();
